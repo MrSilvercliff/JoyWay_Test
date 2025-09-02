ac6a5b3fd143c6b101d7351ec08b8a54bfafa15d
Mikhail <>
2025-09-02 18:06:45 +0300

    - инсталлер для игровой сцены

---
+++ JoyWayTest/Assets/_Project/Scripts/GameScene/Scene/GameSceneInstaller.cs
@@ -0,0 +1,12 @@
+using Assets._Project.Scripts.Project.Zenject;
+using UnityEngine;
+
+namespace Assets._Project.Scripts.GameScene.Scene
+{
+    public class GameSceneInstaller : SceneInstallerAbstract
+    {
+        protected override void OnInstallBindings()
+        {
+        }
+    }
+}

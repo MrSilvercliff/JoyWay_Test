699d7191d9a2492f6d44cc42155d2a370302eff5
Mikhail <>
2025-09-02 17:15:52 +0300

    - project installer для project context

---
+++ JoyWayTest/Assets/_Project/Scripts/Project/Zenject/ProjectInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+namespace Assets._Project.Scripts.Project.Zenject
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
